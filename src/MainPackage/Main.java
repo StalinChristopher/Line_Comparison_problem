e5548a6dc412a9569a30c98e3fd8cbdfc5b85347
Stalin CD <stalincd@120AMBBLR-Samyad-R-Jain.local>
2021-08-22 15:32:28 +0530

    Added method to calc length of line between points given

--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -4,7 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the line Computation program");
-
+		
+		//UC1
+		Point p1=new Point(10,10); //First point of the line
+		Point p2=new Point(20,20); //Second point of the line
+		
+		double length = p2.Linelength(p1);
+		System.out.printf("\nLength of the line between point p1(%.2f,%.2f) and p2(%.2f,%.2f) : %.2f",p1.x,p1.y,p2.x,p2.y,length);
+		
 	}
 
 }
--- src/MainPackage/Point.java
+++ src/MainPackage/Point.java
@@ -1,5 +1,19 @@
 package MainPackage;
 
 public class Point {
-
+	double x;
+	double y;
+	Point(double x,double y){
+		this.x=x;
+		this.y=y;
+	}
+	
+	double Linelength(Point p) {
+		double x1,y1,x2,y2;
+		x1=this.x;
+		y1=this.y;
+		x2=p.x;
+		y2=p.y;
+		return Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
+	}
 }
